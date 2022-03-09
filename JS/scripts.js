e4efaadb6f95c376b9c7d7fcb1f90f0649426cd2
aust1inn <austinotieno364@gmail.com>
2022-03-09 18:57:10 +0300

    test:tests the hover effect

--- JS/scripts.js
+++ JS/scripts.js
@@ -11,3 +11,12 @@ $(document).ready(function() {
         $(".project-management").find("p").toggle();
     });
 });
+
+$(document).ready(function() {
+    $(".img1").hover(function() {
+        $(".img1").find(".overlay").css("display" , "block");
+    },
+    function () {
+        $(".img1").find(".overlay").css("display" , "none");
+    });
+});
